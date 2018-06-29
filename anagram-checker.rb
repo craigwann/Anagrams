5994590e6675130dd0676b7d2f3d3ef102ad4263
craigwann <craigwann@me.com>
2018-06-29 15:14:24 -0700

    add loop to repeat or exit anagram-checker.rb

--- anagram-checker.rb
+++ anagram-checker.rb
@@ -1,9 +1,17 @@
 require('./lib/anagrams.rb')
 
-puts("Anagram checker: enter first half of anagram word or phrase")
-word1 = gets()
-puts("enter 2nd word or phrase")
-word2 = gets()
+loop do
+  puts("Anagram checker: enter first half of anagram word or phrase")
+  word1 = gets()
+  puts("enter 2nd word or phrase")
+  word2 = gets()
 
-anagram = Anagram.new(word1, word2)
-puts("is it an anagram? ", anagram.is_it_anagram())
+  anagram = Anagram.new(word1, word2)
+  puts("is it an anagram? ", anagram.is_it_anagram())
+  puts("Do you have another anagram to check? (Y/N)")
+
+  endloop = gets().chomp().downcase()
+  if endloop == 'n'
+    break
+  end
+end
